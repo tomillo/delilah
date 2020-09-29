0df5654bad4d2354edc7b6cb0468e68c68aaeb44
josefinabortoletto <josefina.bortoletto@gmail.com>
2020-09-28 22:10:43 -0300

    TOTALPEDIDO
    actualiza total pedido, queda comentada la parte de sumar en cantidad si ya existe un producto con ese id en la orden, la suma algebraica no me funciona

--- routes/orders.js
+++ routes/orders.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const moment = require('moment');
 const privateKey = "112358";
 const db = require("../db/mysql_connection");
+const Sequelize = require('sequelize');
 
 function authenticateUser(req, res, next) {
     try {
@@ -14,7 +15,7 @@ function authenticateUser(req, res, next) {
             req.token = bearerToken;
             next();
         } else {
-            res.status(401).json({error: 'Error en verificar el token'})
+            res.status(401).json({ error: 'Error en verificar el token' })
         }
 
     } catch {
@@ -29,7 +30,7 @@ router.get('/', authenticateUser, (req, res) => {
             res.status(401).json('Error en verificar el token');
         } else if (authData.role == '3') {
             res.status(401).json('No esta autorizado a realizar la consulta');
-        } else if (authData.role == '1'){
+        } else if (authData.role == '1') {
             db.sequelize.query(`SELECT u.user, u.name, u.last_name, u.phone, u.address, pm.description AS payment_method, 
                         os.description AS order_status, p.product_name, od.quantity o.total_order
                         FROM users u
@@ -45,7 +46,7 @@ router.get('/', authenticateUser, (req, res) => {
                     // logging: console.log
                 }
             ).then(result => res.json(result));
-        }else{
+        } else {
             db.sequelize.query(`SELECT u.user, u.name, u.last_name, u.phone, u.address, pm.description AS payment_method, 
                         os.description AS order_status, p.product_name, o.total_order
                         FROM users u
@@ -176,16 +177,50 @@ router.post('/', authenticateUser, async (req, res) => {
             ).then(result => result);
 
             if (registeredOrder.length != 0) {
-                // AGREGO PRODUCTO A ORDERS_dETAIL
-                db.query(`
+
+                const isProduct = await db.sequelize.query(`
+                SELECT od.quantity 
+                FROM order_detail od
+                WHERE od.id_product = '${productId}'
+                AND od.id_order = '${registeredOrder[0].id}'`,
+                    {
+                        type: db.Sequelize.QueryTypes.SELECT,
+                        raw: true,
+                        plain: false,
+                        // logging: console.log
+                    }
+                ).then(result => result);
+                console.log(isProduct[0].quantity);
+                console.log(quantity);
+
+                // if (isProduct.length != 0) {
+                //     await db.query(`
+                //         UPDATE order_detail od 
+                //         SET od.quantity = ('${isProduct[0].quantity}' + '${quantity}')
+                //         WHERE od.id = '${productId}'
+                //         AND od.id_order = '${registeredOrder[0].id}'
+                //     `);
+                //     } else {}
+                    // AGREGO PRODUCTO A ORDERS_dETAIL
+                    db.query(`
                     INSERT INTO order_detail(id_order, id_product, quantity)
                     VALUES('${registeredOrder[0].id}', '${productId}', ${quantity})
                 `);
+                
 
                 // ACTUALIZO LA FECHA DE MODIFICACION DE ORDER
                 db.query(`
                     UPDATE orders o 
                     SET modification_date = '${dateOrder_mod}'
+                    WHERE o.id = ${registeredOrder[0].id}
+                `);
+
+                //ACTUALIZO TOTAL PEDIDO 
+                db.query(`
+                    UPDATE orders o 
+                    SET total_order = (SELECT SUM(od.quantity*p.price) FROM order_detail od
+                    INNER JOIN products p ON p.id = od.id_product 
+                    WHERE od.id_order = ${registeredOrder[0].id})
                     WHERE o.id = ${registeredOrder[0].id}
                 `);
 
@@ -228,7 +263,7 @@ router.post('/', authenticateUser, async (req, res) => {
                 db.query(`
                     INSERT INTO order_detail(id_order, id_product, quantity) 
                     VALUES('${newOrder[0].id}', '${productId}', '${quantity}')
-                `);                             
+                `);
             }
         }
     })
